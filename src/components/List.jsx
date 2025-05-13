caeb1812a47b5d424a64331ae5c6ee7a0b2cc9dd
abuzarraziqgithub <abuzarraziqo@gmail.com>
2025-05-13 23:12:25 +0500

    Update styles for todo box and list component; enhance responsiveness and scrollbar behavior

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -7,6 +7,13 @@ export default function List() {
           <li>This is Task 1</li>
           <li>This is Task 2</li>
           <li>This is Task 3</li>
+          <li>This is Task 3</li>
+          <li>This is Task 3</li>
+          <li>This is Task 3</li>
+          <li>This is Task 3</li>
+          <li>This is Task 3</li>
+          <li>This is Task 3</li>
+          <li>This is Task 3</li>
         </ul>
       </div>
     </>
